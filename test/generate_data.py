311924ecf7a911efc7272d9fe5fb7067111e9162
rishivengala11 <vengalarishi143@gmail.com>
2026-09-21 15:26:49 +0530

    updated test/generate_data.py file

--- test/generate_data.py
+++ test/generate_data.py
@@ -1,3 +1,24 @@
+import csv
 APP="Instagram"
-time_spent_last_week = [95,120,80,140,60,170,110]
-counter=sum(time_spent_last_week)
+minutes = []
+with open("digital_behaviour.csv", "r", newline="", encoding="utf-8") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        minutes.append(int(row["Instagram_Minutes"]))
+minutes = minutes[:7]
+# s="A single piece of text"    slicing with step
+# print(s[1::2]+"\t"+s[0::2])
+total=sum(minutes)
+# print(total)
+average=total/len(minutes)
+# print(average)
+highest=max(minutes)
+# print(highest)
+lowest=min(minutes)
+# print(lowest)
+counter=0
+for i in range(len(minutes)):
+    if minutes[i]>average:
+        counter+=1
+# print(counter)
+print(f"The app we are working on is {APP} the total time spent on the app for a week is {total} with an average of {average}.The highest time spent is {highest} and the lowest time spent is   {lowest}.There are {counter} days which are higher than average in the following week.")
